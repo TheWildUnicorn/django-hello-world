928cf119bf8b4951a7c434078b449face5733e1b
Jaydon Carling <jaydon134@me.com>
2017-04-01 20:52:20 +0000

    Template is working. Image in template is not.

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from lxml import html
+import requests
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hi there...")
+	page = requests.get('https://xkcd.com/780/')
+	tree = html.fromstring(page.content)
+	comic = tree.xpath('//div[@id="comic"]/text()')
+	context = {'comic': comic}
+	return render(request, 'hello/index.html', context)
+
